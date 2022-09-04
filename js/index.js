0093b9334215af93ff7f3c82c8eb0235f478df1c
ArrowHead <69950692+ArrowHeadtt@users.noreply.github.com>
2022-09-04 12:09:34 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,3 +5,15 @@ function openLeftMenu() {
 function closeLeftMenu() {
     document.getElementById("leftMenu").style.display = "none";
 }
+function gotoProduce(){
+    window.location.href = "produce.html";
+}
+function gotoMoresoft(){
+    window.location.href = "moresoft.html";
+}
+function gotoApidocs(){
+    window.location.href = "apidocs.html";
+}
+function gotoDownload(){
+    window.location.href = "https://github.com/ArrowHeadtt/ArrowHeadtt.github.io/raw/main/Test.zip";
+}
